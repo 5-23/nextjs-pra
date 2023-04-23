aa33437cb3038912b5397bb02b52bc2ff1b1678f
5-23 <yhanbyeol6@gmail.com>
2023-04-23 22:56:30 +0900

    init

---
+++ app/dev/page.js
@@ -0,0 +1,6 @@
+import style from "./page.module.css"
+export default function Page(){
+    return (
+        <h1 className={style.h1}>dev</h1>
+    )
+} 
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import style from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <h1 className={style.h1}>hi</h1>
+    </main>
+  )
+}
