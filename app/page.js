0f04931c6e4b65aa74f447350f25ded0b372fa1c
5-23 <yhanbyeol6@gmail.com>
2023-04-24 23:18:26 +0900

    make state

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+"use client"
+import { useState } from 'react';
 import style from './page.module.css'
 
 export default function Home() {
+  let [a, cgA] = useState(0);
   return (
     <main className={style.main}>
-      <h1 className={style.h1}>hi</h1>
+      <Asdf name="one"/>
+      <Asdf name="two"/>
+      <Asdf name="three"/>
     </main>
+  )
+}
+
+
+function Asdf({name}){
+  let [a, cgA] = useState(0);
+  return (
+    <div>
+      <h1>{name}: {a}</h1>
+      <input type='button' value={"+"} onClick={() => { cgA(a + 1) }}/>
+    </div>
   )
 }
